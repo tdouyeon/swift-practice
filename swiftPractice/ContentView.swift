76271ec563bc91fdfc9b997d83315c92ee135615
tdouyeon <tdouyeon@gmail.com>
2024-08-07 11:59:56 +0900

    텍스트 추가 및 백그라운드, 라운드 스타일 적용

--- swiftPractice/ContentView.swift
+++ swiftPractice/ContentView.swift
@@ -10,10 +10,13 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+//            Image(systemName: "globe")
+//                .imageScale(.large)
+//                .foregroundStyle(.tint)
+            Text("안녕!!!!").padding()             .background(Color.yellow, in:              RoundedRectangle(cornerRadius: 8))
+            Text("뭐예요?")
+                           .padding()
+                           .background(Color.teal, in: RoundedRectangle(cornerRadius: 8))
         }
         .padding()
     }
