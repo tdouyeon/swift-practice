c15d8f4be25ecd75da1a972f0c789042b5e7562e
tdouyeon <tdouyeon@gmail.com>
2024-08-07 16:57:03 +0900

    ContentView 안에 있는 ButtonView 삭제, 전체적인 UI 변경

--- swiftPractice/ContentView.swift
+++ swiftPractice/ContentView.swift
@@ -7,32 +7,26 @@
 
 import SwiftUI
 
+let gradientColors: [Color] = [
+    .gradientTop,
+    .gradientBottom
+]
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            HStack {
-                buttonView(text:"안녕!!!!",color:Color.yellow)
-                buttonView(text:"뭐예요?",color:Color.teal)
-            }.padding()
-        }
-
+        TabView {
         HStack {
             weatherView(day: "Mon", isRainy: false, color:Color.yellow,temp:[50,70])
             weatherView(day: "Tue", isRainy: true, color:Color.blue, temp:[40,60])
         }
+            WelcomePage()
+            FeaturesPage()
+        }.tabViewStyle(.page).background(Gradient(colors: gradientColors))
     }
 }
 
 #Preview {
     ContentView()
-}
-
-struct buttonView: View {
-    let text:String;
-    let color:Color;
-    var body: some View {
-        Text(text).padding().background(color, in:RoundedRectangle(cornerRadius: 8))
-    }
 }
 
 struct weatherView: View {
