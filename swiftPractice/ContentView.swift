02ff44ca2548057687c5cb51b9b88cbeba835949
tdouyeon <tdouyeon@gmail.com>
2024-08-07 14:30:34 +0900

    날씨 추가 및 구조체 만듦

--- swiftPractice/ContentView.swift
+++ swiftPractice/ContentView.swift
@@ -10,18 +10,46 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-//            Image(systemName: "globe")
-//                .imageScale(.large)
-//                .foregroundStyle(.tint)
-            Text("안녕!!!!").padding()             .background(Color.yellow, in:              RoundedRectangle(cornerRadius: 8))
-            Text("뭐예요?")
-                           .padding()
-                           .background(Color.teal, in: RoundedRectangle(cornerRadius: 8))
+            HStack {
+                buttonView(text:"안녕!!!!",color:Color.yellow)
+                buttonView(text:"뭐예요?",color:Color.teal)
+            }.padding()
         }
-        .padding()
+
+        HStack {
+            weatherView(day: "Mon", isRainy: false, color:Color.yellow,temp:[50,70])
+            weatherView(day: "Tue", isRainy: true, color:Color.blue, temp:[40,60])
+        }
     }
 }
 
 #Preview {
     ContentView()
+}
+
+struct buttonView: View {
+    let text:String;
+    let color:Color;
+    var body: some View {
+        Text(text).padding().background(color, in:RoundedRectangle(cornerRadius: 8))
+    }
+}
+
+struct weatherView: View {
+    let day:String;
+    let isRainy:Bool;
+    let color:Color;
+    let temp: [Int];
+    
+    var body: some View {
+        VStack {
+            Text(day).font(Font.headline)
+            Image(systemName: isRainy ? "cloud.rain.fill" : "sun.max.fill").padding(5)
+
+                .foregroundStyle(color)
+            Text("High: \(temp[0])").fontWeight(.semibold)
+            Text("Low: \(temp[1])").fontWeight(.medium).foregroundStyle(Color.secondary)
+        }
+        .padding()
+    }
 }
