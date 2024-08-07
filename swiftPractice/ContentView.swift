bf8e0f515016e3bb059062c05e167a4d4073c68a
tdouyeon <tdouyeon@gmail.com>
2024-08-07 17:52:23 +0900

    다이스 화면 추가

--- swiftPractice/ContentView.swift
+++ swiftPractice/ContentView.swift
@@ -12,13 +12,36 @@ let gradientColors: [Color] = [
     .gradientBottom
 ]
 
+
 struct ContentView: View {
+    @State private var numberOfDice: Int = 1
+
     var body: some View {
         TabView {
-        HStack {
-            weatherView(day: "Mon", isRainy: false, color:Color.yellow,temp:[50,70])
-            weatherView(day: "Tue", isRainy: true, color:Color.blue, temp:[40,60])
-        }
+            VStack {
+                Text("Dice Roller").font(.largeTitle.lowercaseSmallCaps())
+                HStack {
+                    ForEach(1...numberOfDice, id: \.description) { _ in
+                        DiceView()
+                    }
+                }
+                HStack {
+                    Button("Remove Dice", systemImage: "minus.circle.fill") {
+                        numberOfDice -= 1
+                    }
+                    .disabled(numberOfDice == 1)
+                    Button("Add Dice", systemImage: "plus.circle.fill") {
+                        numberOfDice += 1
+                    }
+                    .disabled(numberOfDice == 3)
+                }
+                .padding().labelStyle(.iconOnly)
+
+            }
+            HStack {
+                weatherView(day: "Mon", isRainy: false, color:Color.yellow,temp:[50,70])
+                weatherView(day: "Tue", isRainy: true, color:Color.blue, temp:[40,60])
+            }
             WelcomePage()
             FeaturesPage()
         }.tabViewStyle(.page).background(Gradient(colors: gradientColors))
