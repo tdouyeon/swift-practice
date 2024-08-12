11df90feec8b156f84aeda3b0bc8697ec5055371
tdouyeon <tdouyeon@gmail.com>
2024-08-12 16:58:52 +0900

    Initial Commit

---
+++ swift-practice/swift_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_practiceApp.swift
+//  swift-practice
+//
+//  Created by suyeon on 8/12/24.
+//
+
+import SwiftUI
+
+@main
+struct swift_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
